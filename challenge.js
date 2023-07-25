33ae969ab82c6e1bb994948e7f34168a62baa803
Rayyan365 <rayanabdallah09@gamil.com>
2023-07-25 00:41:20 -0700

    rock-paper-scissors done

--- challenge.js
+++ challenge.js
@@ -1,2 +1,19 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+function playGame(player1, player2)if (
+      (player1 === "rock" && player2 === "scissors") 
+      (player1 === "paper" && player2 === "rock")
+      (player1 === "scissors" && player2 === "paper")
+    ) {
+      return "player1 wins";
+    } else if 
+        (player1 === "rock" && player2== "rock")
+        (player1 === "scissors" && player2== "scissors")
+        (player1 === "paper" && player2== "paper") 
+          return "tie";
+        
+    else  {
+      return "player2 wins";
+    }
+  
+  
